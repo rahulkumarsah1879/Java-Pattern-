da3f5ed91c52aa261ebf0666a7649e667def17a0
Rahul Kumar Sah <99410577+rahulkumarsah1879@users.noreply.github.com>
2023-10-26 09:27:46 +0530

    Add files via upload

---
+++ HalfPyramidNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class HalfPyramidNumber {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a=scan.nextInt();
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
